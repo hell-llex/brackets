c921677dbd519a3ffef310f1ba0e774a8d4cc806
hell_llex <demeshenko333@gmail.com>
2022-02-23 21:30:40 +0300

    complete task!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [] 
-  bra = new Map(bracketsConfig);
+  let bra = new Map(bracketsConfig);
   for (let i = 0; i < str.length; i++) {
     let x = str[i]
     if (bra.has(str[0])) {
